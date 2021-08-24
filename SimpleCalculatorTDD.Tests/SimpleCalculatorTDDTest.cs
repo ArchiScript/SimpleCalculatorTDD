252b7eed6909f79e4a4f0e7519e8a67318d37ef1
Артур <arturskomarohov@gmail.com>
2021-08-24 10:05:17 +0300

    Добавьте файлы проекта.

---
+++ SimpleCalculatorTDD.Tests/SimpleCalculatorTDDTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xunit;
+using SimpleCalculatorTDD;
+
+namespace SimpleCalculatorTDD.Tests
+{
+    public class SimpleCalculatorTDDTest
+    {
+        [Fact]
+        public void GetSum_3_Plus_2_Eq_5()
+        {
+            //Arrange 
+            SimpleCalculatorTDDClass calc = new SimpleCalculatorTDDClass();
+
+            //Act
+            int result = calc.GetSum(3, 2);
+
+            //Assert
+            Assert.Equal(5, result);
+        }
+        [Fact]
+        public void GetSubstract_20_Minus_14_Eq_6()
+        {
+            //Arrange 
+            SimpleCalculatorTDDClass calc = new SimpleCalculatorTDDClass();
+
+            //Act
+            int result = calc.GetSubstract(20, 14);
+
+            //Assert
+            Assert.Equal(6, result);
+        }
+        [Fact]
+        public void GetMultiply_7_Mult_8_Eq_56()
+        {
+            //Arrange 
+            SimpleCalculatorTDDClass calc = new SimpleCalculatorTDDClass();
+
+            //Act
+            int result = calc.GetMultiply(7, 8);
+
+            //Assert
+            Assert.Equal(56, result);
+        }
+
+        [Fact]
+        public void GetDivide_42_Div_6_Eq_7()
+        {
+            //Arrange 
+            SimpleCalculatorTDDClass calc = new SimpleCalculatorTDDClass();
+
+            //Act
+            int result = calc.GetDivide(42, 6);
+
+            //Assert
+            Assert.Equal(7, result);
+        }
+    }
+}
---
+++ SimpleCalculatorTDD/SimpleCalculatorTDDClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculatorTDD
+{
+    public class SimpleCalculatorTDDClass
+    {
+        public int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+        public int GetSubstract(int a, int b)
+        {
+            return a - b;
+        }
+        public int GetMultiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int GetDivide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
